dfa635b9b034159e914815ea09e50a383740c118
Luis <BurritoGaiden@users.noreply.github.com>
2018-04-23 19:19:06 -0700

    Cleaning up points of interest mechanic and added it's UI

--- Assets/Interesting.cs
+++ Assets/Interesting.cs
@@ -1,22 +1,33 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Interesting : MonoBehaviour {
 
     public static bool looking;
+    public Image intImage;
 
-	void OnTriggerStay(Collider hit)
-    {
-        if (hit.transform.GetComponent<Interestable>()) {
-            looking = Input.GetKey(KeyCode.E);
-        }
+    void Start() {
+        intImage.enabled = false;
+        looking = false;
     }
-    /*
+    
     void OnTriggerExit(Collider hit) {
-        if (hit.transform.GetComponent<Interestable>()) {
-            canLook = false;
+        if (hit.transform.GetComponent<Interestable>())
+        {
+            looking = false;
+            intImage.enabled = false;
         }
     }
-    */
+
+    void OnTriggerStay(Collider hit)
+    {
+        if (hit.transform.GetComponent<Interestable>())
+        {
+            looking = Input.GetKey(KeyCode.E);
+            intImage.enabled = !looking;
+        }
+    }
+    
 }
