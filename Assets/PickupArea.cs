86abad1dac64f75e399038ff8b288470755ee7c6
Nesfero <thenesfero@gmail.com>
2018-05-10 07:38:19 -0700

    Repo Purge due to weird merge

--- Assets/PickupArea.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class PickupArea : MonoBehaviour {
-
-    public GameObject pickable;
-    public GameObject pickablePlacementPlaceholder;
-}
