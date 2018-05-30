34813f36f8175f88d6e29b6507e7e0b1e3d3cb5d
Nesfero <thenesfero@gmail.com>
2018-05-30 09:05:16 -0700

    Added particle fx, iterating on vehicle appearance section fx

--- Assets/Scripts/CrateDestroyer.cs
+++ Assets/Scripts/CrateDestroyer.cs
@@ -5,11 +5,36 @@ using UnityEngine;
 public class CrateDestroyer : MonoBehaviour {
 
     public GameObject brokenCratePrefab;
+    public bool hasExploded;
+    public GameObject explosion;
+
+    public float radius;
+    public float force;
+    public float explosionRadius;
 
 	// Update is called once per frame
 	public void ExplodeCrate() {
         Instantiate(brokenCratePrefab, transform.position, transform.rotation);
+        Explode();
         Destroy(gameObject);
+    }
+
+    public void Explode() {
+        if (explosion != null)
+        {
+            Instantiate(explosion, transform.position, transform.rotation);
+
+            Collider[] colliders = Physics.OverlapSphere(transform.position, radius);
+
+            foreach (Collider nearbyCrates in colliders)
+            {
+                Rigidbody rb = nearbyCrates.GetComponent<Rigidbody>();
+                if (rb != null)
+                {
+                    rb.AddExplosionForce(force, transform.position, radius);
+                }
+            }
+        }
     }
 	
 }
